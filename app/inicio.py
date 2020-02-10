64123625d6fea4038bd90036e2939b1703fb89c0
Murilo Moreira Noguerol <67069@proway.treina>
2020-02-10 20:34:38 -0300

    aula 1

---
+++ app/inicio.py
@@ -0,0 +1,21 @@
+#imprmir info na tela #(comentario)
+CNTRLK  CNTRLC
+#print('/n') #e pular linha
+#print('*' 50 )
+#print("olá joelma do calipso")
+#print("olá chimbinha do calipso")
+#print("/n")
+
+
+# pegar entrada do usuario
+input('digite seu nome')
+input('digite seu sobrenome')
+
+
+#usando format para concatenaçao de string
+print('ola',format( nome,sobrenome))
+
+#interpolaçao de string
+print( f'ola {nome} {sobrenome}')
+
+ 
